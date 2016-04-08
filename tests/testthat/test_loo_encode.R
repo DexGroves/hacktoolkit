c6c372dccea9a2ab63a82087a13b82536467c6de
Groves <Declan.Groves@allstate.com>
2016-04-08 09:07:46 -0500

    Test for leave-one-out encoding

---
+++ tests/testthat/test_loo_encode.R
@@ -0,0 +1,14 @@
+# context("loo_encode")
+
+
+# test_that("loo_encode behaving as expected", {
+#   test_data <- data.frame(
+#     id = c(rep("a", 5), rep("b", 3), rep("c", 2), "d"),
+#     resp = c(1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1)
+#   )
+
+#   encoding <- loo_encode(test_data$id, test_data$resp)
+#   expected_encoding <- c(0, 0.8, 0.8, 0.8, 0.8, 0, 0.5, 0.5, 0, 1, NA)
+
+#   expect_equal(encoding, expected_encoding)
+# })
